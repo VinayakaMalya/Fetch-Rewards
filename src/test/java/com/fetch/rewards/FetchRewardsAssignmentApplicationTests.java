ebc4b7fda64437526fe81cc515d6df9aa7b9f279
Malya <vinmalya@UMICH.EDU>
2021-03-21 00:07:48 -0400

    First Checkin

---
+++ src/test/java/com/fetch/rewards/FetchRewardsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetch.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchRewardsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
